884421455f3076302893f1822c8d7891294faaa8
Lucas Gaparino <lucas.gasparino@bsc.es>
2022-08-19 11:21:46 +0200

    New master file and gitignore.

--- test_mesh.cpp
+++ test_mesh.cpp
@@ -1,54 +1,23 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
 #include <cstdint>
 
 #include "Point.h"
 #include "Element.h"
 #include "Mesh.h"
 
-int main(void)
+int main(int argc, char const *argv[])
 {
-    // Create 4 nodes
-    std::vector<Point> p;
-    p.push_back(Point(0, {0.0, 0.0}));
-    p.push_back(Point(1, {1.0, 0.0}));
-    p.push_back(Point(2, {1.0, 1.0}));
-    p.push_back(Point(3, {0.0, 1.0}));
-
-    // Create a QUA04 with the 4 nodes
-    std::vector<Element> e;
-    e.push_back(Element(0,p));
-
-    // Create a grid and insert the nodes  and elements
-    Mesh m;
-    for (uint64_t i = 0; i < 4; i++)
+    if (argc != 2)
     {
-        m.buildNodesList(i,p[i].getNodeXYZ());
-    }
-    for (uint64_t i = 0; i < 1; i++)
-    {
-        m.buildElementsList(i,e[i].getElementNodes());
+        std::cout << "Usage: " << argv[0] << " <mesh_file>" << std::endl;
+        return 1;
     }
 
-    // Extract element from mesh and probe it
-    Element e0 = m.getMeshElementFromID(0);
-    std::cout << "--|e0 inforrmation : " << std::endl;
-    std::cout << "  --|ID: " << e0.getElementID() << " " << std::endl;
-    std::cout << "  --|Type: " << e0.getElementType() << " " << std::endl;
-    std::cout << "  --|Nodes: " << e0.getElementNumNodes() <<std::endl;
-
-    // Extract node 2 from mesh and probe it
-    Point p2 = m.getMeshNodeFromID(2);
-    std::vector<double> xyz = p2.getNodeXYZ();
-    std::cout << "--|p2 inforrmation : " << std::endl;
-    std::cout << "  --|ID: " << p2.getNodeID() << " " << std::endl;
-    std::cout << "  --|XYZ: ";
-    for (uint64_t i = 0; i < xyz.size(); i++)
-    {
-        std::cout << xyz[i] << " ";
-    }
-    std::cout<<std::endl;
-    
-    // End test
+    // Read argument into fileName string and appennd extension
+    std::string fileName = argv[1];
+    fileName.append(".geo.dat");
     return 0;
 }
